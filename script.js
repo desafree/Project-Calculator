b84da8ec4808eb1b8a39eb39f31c943f8b141d47
Nicola De Sanctis <nicoladesanctis99@gmail.com>
2021-10-08 23:56:17 +0200

    update code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let text = '';
-let operatore;
+let operatore = [];
 let numbers = [];
+let risultato;
 
 const display = document.querySelector('.display');
 
@@ -10,29 +11,84 @@ const number = document.querySelectorAll('.number');
 for(let i=0; i< number.length; i++) {
     number[i].addEventListener('click', function(e) {
         text += e.target.innerText;
-        display.textContent=text;
+        printdisplay(text);
     })
 }
 
 //activate function mechanism
 const functions = document.querySelectorAll('.function');
+
 for(let h=0; h<functions.length; h++) {
     functions[h].addEventListener('click', function(e) {
-        operatore = e.target.innerText;
-        if(numbers.length == 0) {
-            numbers[0] = parseInt(text);
-            text = '';
-            display.textContent=text;
-        }
-        else if(numbers.length == 1) {
-            numbers[1] = parseInt(text);
-            text = '';
-            display.textContent=text;
-        }
-        console.log(text);
+        operatore[operatore.length] = e.target.innerText;
+        console.log(operatore)
+        numbers[numbers.length] = parseInt(text);
         console.log(numbers)
-    })
-}
+        text='';
+        if(numbers.length == 2) {
+            risultato = operation(numbers[0],numbers[1],operatore[0]);
+            printdisplay(risultato);
+            console.log(risultato)
+            let a =operatore[1];
+            operatore = [];
+            operatore[0]=a;
+            numbers = [];
+            numbers[0] = risultato
+            text = '';
+        }
+        
+        
+       
+    })}
+
+
+const uguale = document.querySelector('.uguale');
+uguale.addEventListener('click', function() {
+    let primo = numbers[0];
+    let secondo = parseInt(text);
+    let opera = operatore[0];
+    let nico = operation(primo,secondo,opera);
+    printdisplay(nico);
+    numbers = [];
+    operatore = [];
+    text=nico;
+})
+
+
+const clean = document.querySelector('.clear');
+clean.addEventListener('click', function() {
+    clear();
+    printdisplay(text)
+})
+
+
+
+// for(let h=0; h<functions.length; h++) {
+//     functions[h].addEventListener('click', function(e) {
+//         operatore = e.target.innerText;
+//         if(numbers.length == 0) {
+//             numbers[0] = parseInt(text);
+//             text = '';
+//             printdisplay(text);
+//         }
+//         else if(numbers.length == 1) {
+//             numbers[1] = parseInt(text);
+//             text = '';
+//             printdisplay(text);
+//             console.log(operatore);
+//             console.log(numbers[0]);
+//             console.log(numbers[1]);
+//             numbers[0]= operation(numbers[0],numbers[1],operatore);
+//             console.log(operatore);
+//             console.log(numbers[0]);
+//             console.log(numbers[1]);
+//             numbers.splice(1,1);
+//             printdisplay(text);
+            
+//         }
+        
+//     })
+// }
 
 
 
@@ -66,35 +122,52 @@ for(let h=0; h<functions.length; h++) {
 
 
 function division(number1,number2) {
-    return number1/number2;
+    return Math.round((number1/number2) * 100) / 100;
 }
 
 function moltiplication(number1,number2) {
-    return number1*number2;
+    return Math.round((number1*number2) * 100) / 100;
 }
 
 function subtraction(number1,number2) {
-    return number1-number2;
+    return Math.round((number1-number2) * 100) / 100;
 }
 
 function addition(number1,number2) {
-    return number1+number2;
+    return Math.round((number1+number2) * 100) / 100;
 }
 
-function operation(number1,number1,operator) {
+function operation(number1,number2,operator) {
     if(operator=='+') {
-        return addition();
+        return addition(number1,number2);
     }
     else if(operator=='-') {
-        return subtraction();
+        return subtraction(number1,number2);
     }
     else if(operator=='*') {
-        return moltiplication();
+        return moltiplication(number1,number2);
     }
     else if(operator=='/') {
-        return division();
+        return division(number1,number2);
     }
     else {
-        alert('error operator');
+        alert('operator error') 
     }
 }
+
+function printdisplay(texts) {
+    if(texts=='') {
+        display.textContent=numbers[0];
+    }
+    else {
+        display.textContent=texts;
+    }
+}
+
+ function clear() {
+    
+    operatore = [];
+    numbers = [];
+    text = '0'
+
+ }
